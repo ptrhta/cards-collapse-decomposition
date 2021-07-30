21cd4a95f2cbb20222abeef6bc91b45c1c2e2a15
ptrhta <49519262+ptrhta@users.noreply.github.com>
2021-07-30 16:28:02 +0300

    Update App.jsx

--- cards/src/App.jsx
+++ cards/src/App.jsx
@@ -9,14 +9,14 @@ function App() {
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+          <a href="/" className="btn btn-primary">Go somewhere</a>
         </div>
     </Card>
     <Card>
       <div className="card-body">
         <h5 className="card-title">Special title treatment</h5>
         <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+        <a href="/" className="btn btn-primary">Go somewhere</a>
       </div>
     </Card>
     </>
